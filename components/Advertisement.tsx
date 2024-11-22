54f5e54cd60719bc7abdb9caffb6364b804fca74
J-CamiloG <jc6381878@gmail.com>
2024-11-21 20:05:07 -0500

    Feat : Se integro una Seccion para mensaje

---
+++ components/Advertisement.tsx
@@ -0,0 +1,9 @@
+export default function Advertisement() {
+    return (
+        <div className="bg-gradient-to-r from-purple-800 to-pink-600 rounded-lg p-4 text-white mb-8">
+            <h3 className="text-2xl font-bold mb-2">Ritmika</h3>
+            <p className="mb-4">This design is part of a technical test. Some of the features we see here, like the library sections, are things I like, while other functionalities are still in beta and not yet fully operational. The main functionality of the page is to search for songs, albums, or artists, view their details, and more — all of which works normally. The rest is part of a future integration.</p>
+            <button className="bg-white text-black px-4 py-2 rounded-full font-bold">Read</button>
+        </div>
+    );
+}
